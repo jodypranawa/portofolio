c9c5d2a241bb5d2ffe0e15f0530ca44506d2c8b1
jodypranawa <jodypranawa@gmail.com>
2026-01-27 10:23:29 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const toggleTheme = document.getElementById("toggleTheme");
+
+toggleTheme.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  toggleTheme.textContent =
+    document.body.classList.contains("dark") ? "🌙" : "☀️";
+});
